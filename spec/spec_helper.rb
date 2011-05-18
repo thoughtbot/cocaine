f3ba33c6f6dbbf1c44df5c506642826980ed7250
Jon Yurek <jyurek@thoughtbot.com>
2011-05-18 15:00:49 -0400

    Treat with_exitstatus right

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'rspec'
+require 'spec/support/with_exitstatus'
 require 'mocha'
 require 'bourne'
 require 'cocaine'
 
 RSpec.configure do |config|
   config.mock_with :mocha
+  config.include WithExitstatus
 end
---
+++ spec/support/with_exitstatus.rb
@@ -0,0 +1,12 @@
+module WithExitstatus
+  def with_exitstatus_returning(code)
+    saved_exitstatus = $?.nil? ? 0 : $?.exitstatus
+    begin
+      `ruby -e 'exit #{code.to_i}'`
+      yield
+    ensure
+      `ruby -e 'exit #{saved_exitstatus.to_i}'`
+    end
+  end
+end
+
