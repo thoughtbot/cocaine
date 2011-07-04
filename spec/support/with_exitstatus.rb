dee7c1c3539583bf2d5c4ecfb0dd888ac08b151b
Jon Yurek <jyurek@thoughtbot.com>
2011-07-04 17:17:18 -0400

    Bump rake version, use double quotes if possible.

--- spec/support/with_exitstatus.rb
+++ spec/support/with_exitstatus.rb
@@ -2,10 +2,10 @@ module WithExitstatus
   def with_exitstatus_returning(code)
     saved_exitstatus = $?.nil? ? 0 : $?.exitstatus
     begin
-      `ruby -e 'exit #{code.to_i}'`
+      `ruby -e "exit #{code.to_i}"`
       yield
     ensure
-      `ruby -e 'exit #{saved_exitstatus.to_i}'`
+      `ruby -e "exit #{saved_exitstatus.to_i}"`
     end
   end
 end
