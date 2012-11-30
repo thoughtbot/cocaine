3d22d465ca61c8e939072f0ce7508ca0d981b14f
Joshua Clayton <jclayton@thoughtbot.com>
2012-11-30 10:34:25 -0500

    Use climate_control to manage environment

--- cocaine.gemspec
+++ cocaine.gemspec
@@ -16,6 +16,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
+  s.add_dependency('climate_control', '>= 0.0.3')
   s.add_development_dependency('rspec')
   s.add_development_dependency('bourne')
   s.add_development_dependency('mocha')
--- lib/cocaine/command_line/runners/backticks_runner.rb
+++ lib/cocaine/command_line/runners/backticks_runner.rb
@@ -1,24 +1,14 @@
 # coding: UTF-8
 
+require 'climate_control'
+
 module Cocaine
   class CommandLine
     class BackticksRunner
 
       def call(command, env = {})
-        with_modified_environment(env) do
+        ClimateControl.modify(env) do
           `#{command}`
-        end
-      end
-
-      private
-
-      def with_modified_environment(env)
-        begin
-          saved_env = ENV.to_hash
-          ENV.update(env)
-          yield
-        ensure
-          ENV.update(saved_env)
         end
       end
 
