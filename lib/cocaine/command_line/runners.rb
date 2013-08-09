4cae4ecc9eb03ebe65c2073bafdff38502195da4
Jon Yurek <jyurek@thoughtbot.com>
2013-08-09 15:06:27 -0400

    Add a runner that uses IO.popen

--- lib/cocaine/command_line/runners.rb
+++ lib/cocaine/command_line/runners.rb
@@ -3,4 +3,5 @@
 require 'cocaine/command_line/runners/backticks_runner'
 require 'cocaine/command_line/runners/process_runner'
 require 'cocaine/command_line/runners/posix_runner'
+require 'cocaine/command_line/runners/popen_runner'
 require 'cocaine/command_line/runners/fake_runner'
---
+++ lib/cocaine/command_line/runners/popen_runner.rb
@@ -0,0 +1,21 @@
+# coding: UTF-8
+
+module Cocaine
+  class CommandLine
+    class PopenRunner
+      def call(command, env = {})
+        with_modified_environment(env) do
+          IO.popen(command, "r") do |pipe|
+            pipe.read
+          end
+        end
+      end
+
+      private
+
+      def with_modified_environment(env, &block)
+        ClimateControl.modify(env, &block)
+      end
+    end
+  end
+end
