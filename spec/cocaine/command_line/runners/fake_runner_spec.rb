4a97ee4fbd662043551fe62d7e0982208c83261b
Jeremy Neander <subjectego@gmail.com>
2014-06-20 13:12:07 -0500

    Use '==' for assertions against true and false
    This fixes tests breaking with non-monkey-patched 'true' and 'false' objects.

--- spec/cocaine/command_line/runners/fake_runner_spec.rb
+++ spec/cocaine/command_line/runners/fake_runner_spec.rb
@@ -10,13 +10,13 @@ describe Cocaine::CommandLine::FakeRunner do
   it 'can tell if a command was run' do
     subject.call("some command", :environment)
     subject.call("other command", :other_environment)
-    subject.ran?("some command").should be_true
-    subject.ran?("no command").should be_false
+    subject.ran?("some command").should == true
+    subject.ran?("no command").should == false
   end
 
   it 'can tell if a command was run even if shell options were set' do
     subject.call("something 2>/dev/null", :environment)
-    subject.ran?("something").should be_true
+    subject.ran?("something").should == true
   end
 
 end
