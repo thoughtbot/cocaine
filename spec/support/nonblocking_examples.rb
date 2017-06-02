64ec3f8ce7dacee6bbfcc37df1371bf8b1cbd429
Mike Mangino <mmangino@elevatedrails.com>
2017-06-02 15:09:38 +0000

    Fix tabs

--- spec/support/nonblocking_examples.rb
+++ spec/support/nonblocking_examples.rb
@@ -1,16 +1,17 @@
 shared_examples_for "a command that does not block" do
-  it 'does not block if the command output a lot on stderr' do
-		cmd = Cocaine::CommandLine.new(
-			"ruby",
-			"-e '$stdout.puts %{hello}; $stderr.puts %{goodbye}*10_000'",
-			:swallow_stderr => false
-		)   
+  it "does not block if the command output a lot on stderr" do
+    cmd = Cocaine::CommandLine.new(
+      "ruby",
+      "-e '$stdout.puts %{hello}; $stderr.puts %{goodbye}*10_000'",
+      :swallow_stderr => false
+    )
     Timeout.timeout(5) do
-			cmd.run
-		end
-		expect(cmd.command_output).to eq "hello\n"
-		expect(cmd.command_error_output).to eq "#{"goodbye" * 10_000}\n"
+      cmd.run
+    end
+    expect(cmd.command_output).to eq "hello\n"
+    expect(cmd.command_error_output).to eq "#{"goodbye" * 10_000}\n"
   end
+
   it 'does not block if the command outputs a lot of data' do
     garbage_file = Tempfile.new("garbage")
     10.times{ garbage_file.write("A" * 1024 * 1024) }
